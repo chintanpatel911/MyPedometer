d35768b30ccf0fc9c73b4bd3f1a6f0d03af01884
Chintan Patel <chintanpatel911@gmail.com>
2018-05-21 09:35:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chintanpatel/mypedometer/AccelerometerData.java
@@ -0,0 +1,13 @@
+package com.example.chintanpatel.mypedometer;
+
+
+public class AccelerometerData {
+
+    public double value;
+    public float x;
+    public float y;
+    public float z;
+    public long time;
+    public boolean isRealPeak = true;
+}
+
---
+++ app/src/main/java/com/example/chintanpatel/mypedometer/DateStepsModel.java
@@ -0,0 +1,8 @@
+package com.example.chintanpatel.mypedometer;
+
+
+public class DateStepsModel {
+
+    public String mDate;
+    public int mStepCount;
+}
